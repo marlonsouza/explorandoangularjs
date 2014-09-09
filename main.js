41378913c6f201d315b5be596573dba918fe8f47
Márlon <marlon.souuza@gmail.com>
2014-09-08 23:28:51 -0300

    Atualizando projeto.

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp',['ngRoute']);
 
 app.directive('marlon', function(){
    return{
        restrict: 'E',
        templateUrl: '/explorandoangularjs/views/marlon.html'
    }
+});
+
+app.config(function($routeProvider){
+   $routeProvider.when('/', {
+        templateUrl:'views/lista_itens.tpl.html'
+   });
+
+   $routeProvider.when('/novoitem', {
+        templateUrl:'views/adiciona_item.tpl.html'
+   });
+
+   $routeProvider.when('/listaitens', {
+        templateUrl:'views/lista_itens.tpl.html'
+   });
 });
