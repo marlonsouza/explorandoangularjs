29d5bb7e925bd97fdde308e28430b60a19a58632
Márlon <marlon.souuza@gmail.com>
2014-09-13 19:02:32 -0300

    Terminando o CRUD.

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,5 @@
 app.controller('itemsController', function ($scope) {
 
-	$scope.addButton = 'Add Item';
-
 	$scope.items = [
 		{nome: 'Sapatênis', qtd: 8, price: 15.90},
 		{nome: 'Tênis', qtd: 12, price: 25.90},
@@ -65,23 +63,15 @@ app.controller('itemsController', function ($scope) {
 		{nome: 'Havaianas', qtd: 10, price: 10.90}
 	];
 
-	$scope.adicionaItem = false;
-
-	$scope.remove = function(index){
+    $scope.remove = function(index){
 		$scope.items.splice(index,1);
 	}
 
-	$scope.habilitaNovo = function(){
-		$scope.adicionaItem = !$scope.adicionaItem;
-
-		if($scope.adicionaItem){
-			$scope.addButton = 'Ocultar';
-		}else{
-			$scope.addButton = 'Add Item';
-		};
-
-        $scope.salvar = montaOperacao(novo);
+	$scope.cadastrarNovo = function(){
+		$scope.salvar = novo();
         $scope.id = 0;
+        console.log($scope.salvar);
+        location.href = './#/novoitem';
 	}
 
     $scope.editar = function(index){
@@ -89,27 +79,28 @@ app.controller('itemsController', function ($scope) {
 
         $scope.newitem = $scope.items[index];
 
-        $scope.salvar = montaOperacao(update);
+        $scope.salvar = update();
 
         $scope.id = index;
+
+        location.href = './#/novoitem';
     }
 
-    var novo = function(index){
-        $scope.items.push($scope.newitem);
-    }
-
-    var update = function(index){
-        $scope.items[index] = $scope.newitem;
-    }
-
-    function montaOperacao(opc){
+    function novo (){
         return function(){
-            opc($scope.id);
-            $scope.newitem = null;
-            $scope.adicionaItem = !$scope.adicionaItem;
+            $scope.items.push($scope.newitem);
+            //$scope.newitem = null;
+            location.href = './#/listaitens';
         }
     }
 
+    function update(){
+        return function(){
+            $scope.items[$scope.id] = $scope.newitem;
+            $scope.newitem = null;
+            location.href = './#/listaitens';
+        }
+    }
 
 });
 
