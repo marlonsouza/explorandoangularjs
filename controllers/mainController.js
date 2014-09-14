3a0ce6a103f6f61c986f0ecc553496251833c27e
Márlon <marlon.souuza@gmail.com>
2014-09-14 17:06:49 -0300

    Finalizando CRUD.

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -26,41 +26,7 @@ app.controller('itemsController', function ($scope) {
 		{nome: 'Tênis', qtd: 12, price: 25.90},
 		{nome: 'Havaianas', qtd: 10, price: 10.90},
 		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90},
-		{nome: 'Sapatênis', qtd: 8, price: 15.90},
-		{nome: 'Tênis', qtd: 12, price: 25.90},
-		{nome: 'Havaianas', qtd: 10, price: 10.90}
+		{nome: 'Tênis', qtd: 12, price: 25.90}
 	];
 
     $scope.remove = function(index){
@@ -68,41 +34,38 @@ app.controller('itemsController', function ($scope) {
 	}
 
 	$scope.cadastrarNovo = function(){
+
 		$scope.salvar = novo();
         $scope.id = 0;
-        console.log($scope.salvar);
+
         location.href = './#/novoitem';
 	}
 
     $scope.editar = function(index){
+
         $scope.adicionaItem = true;
-
         $scope.newitem = $scope.items[index];
-
         $scope.salvar = update();
-
         $scope.id = index;
 
         location.href = './#/novoitem';
     }
 
     function novo (){
-        return function(){
-            $scope.items.push($scope.newitem);
-            //$scope.newitem = null;
+        return function(item){
+            console.log(item);
+            $scope.items.push(item);
+            $scope.newitem = null;
             location.href = './#/listaitens';
         }
     }
 
     function update(){
-        return function(){
-            $scope.items[$scope.id] = $scope.newitem;
+        return function(item){
+            $scope.items[$scope.id] = item;
             $scope.newitem = null;
             location.href = './#/listaitens';
         }
     }
 
 });
-
-
-
